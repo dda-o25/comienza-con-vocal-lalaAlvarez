9773e0883fb68706a8eb1b8a0898587434f75cd8
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-18 18:14:19 -0600

    Listo calisto

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -2,14 +2,21 @@
 Inserta el encabezado aquí y escribe tu código abajo
 """
 
-# Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+palabra = input("Escribe una palabra: ")
+
 
 # Proceso
 
+if ord(palabra[0]) == 65 or ord(palabra[0]) == 69 or ord(palabra[0]) == 73 or  ord(palabra[0]) == 79 or ord(palabra[0]) == 85:
+    salida = "'" + palabra + "' comienza con vocal"
+elif ord(palabra[0]) == 181 or ord(palabra[0]) == 144 or ord(palabra[0]) == 214 or ord(palabra[0]) ==224 or ord(palabra[0]) == 233:
+    salida = "'" + palabra + "' comienza con vocal"
+elif ord(palabra[0]) == 97 or ord(palabra[0]) == 111 or ord(palabra[0]) == 101 or ord(palabra[0]) == 105 or ord(palabra[0]) == 117:
+        salida = "'" + palabra + "' comienza con vocal"
+else:
+    salida = "'" + palabra + "' no comienza con vocal"
 
 # Salidas
 print(salida)

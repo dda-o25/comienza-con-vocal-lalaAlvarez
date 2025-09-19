35c005a3c9d17d258be8b68dfb1aa17fce6b9414
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-18 23:18:49 -0600

    correcion codigo ascii

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -10,19 +10,22 @@ palabra = input("Escribe una palabra: ")
 
 # Proceso
 
+
+#mayusculas
 if ord(palabra[0]) == 65 or ord(palabra[0]) == 69 or ord(palabra[0]) == 73 or  ord(palabra[0]) == 79 or ord(palabra[0]) == 85:
     salida = "'" + palabra + "' comienza con vocal"
-elif  ord(palabra[0]) == 144 or ord(palabra[0]) == 214 or ord(palabra[0]) ==224 or ord(palabra[0]) == 233:
+# minusculas
+elif  ord(palabra[0]) == 97 or ord(palabra[0]) == 101 or ord(palabra[0]) == 105 or ord(palabra[0]) == 111 or ord(palabra[0]) == 117:
     salida = "'" + palabra + "' comienza con vocal"
-elif ord(palabra[0]) == 97 or ord(palabra[0]) == 111 or ord(palabra[0]) == 101 or ord(palabra[0]) == 105 or ord(palabra[0]) == 117:
+# caracteres raros
+elif ord(palabra[0]) >= 129 or ord(palabra[0]) <=134 or ord(palabra[0]) >= 136 or ord(palabra[0]) <=144:
         salida = "'" + palabra + "' comienza con vocal"
-elif ord(palabra[0]) >= 60 or ord(palabra[0]) <= 63 or ord(palabra[0]) >= 129 or ord(palabra[0]) <= 134 or ord(palabra[0]) >= 136 or ord(palabra[0]) <= 144:
+elif ord(palabra[0]) >= 147 or ord(palabra[0]) <= 151 or ord(palabra[0]) >= 153 or ord(palabra[0]) <= 154 or ord(palabra[0]) >= 160 or ord(palabra[0]) <= 163:
     salida = "'" + palabra + "' comienza con vocal"
-elif ord(palabra[0]) >= 147 or ord(palabra[0]) <= 154:
+#mayus raras
+elif ord(palabra[0]) >= 181 or ord(palabra[0]) <= 183 or ord(palabra[0]) == 198 or ord(palabra[0]) == 199:
     salida = "'" + palabra + "' comienza con vocal"
-elif ord(palabra[0]) >= 181 or ord(palabra[0]) <= 183:
-    salida = "'" + palabra + "' comienza con vocal"
-elif ord(palabra[0]) >= 210 or ord(palabra[0]) <= 216:
+elif ord(palabra[0]) >= 210 or ord(palabra[0]) <= 216 or ord(palabra[0]) == 224:
     salida = "'" + palabra + "' comienza con vocal"
 elif ord(palabra[0]) >= 226 or ord(palabra[0]) <= 229:
     salida = "'" + palabra + "' comienza con vocal"
